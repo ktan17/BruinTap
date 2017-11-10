a8f861831e4f83b12f6dc11011b79c7be387ca2e
ktan17 <ktan17@ucla.edu>
2017-11-09 17:23:03 -0800

    initial commit

--- BruinTap/ViewController.swift
+++ BruinTap/ViewController.swift
@@ -10,9 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var score = 0
+    
+    var timer: Timer!
+    var time: Double = 10
+    
+    @IBOutlet var tapButton: UIButton!
+    @IBOutlet var startButton: UIButton!
+    
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func startTap(_ sender: UIButton) {
+        
+        score = 0
+        time = 10
+        
+        scoreLabel.text = String(score)
+        timerLabel.text = String(format: "%.2f", time)
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        
+        startButton.isEnabled = false
+        tapButton.isEnabled = true
+        
+    }
+    
+    @IBAction func buttonTap(_ sender: UIButton) {
+        
+        score += 1
+        scoreLabel.text = String(score)
+        
+    }
+    
+    @objc func timerAction() {
+        
+        time -= 0.01
+        timerLabel.text = String(format: "%.2f", time)
+        
+        if time <= 0 {
+            
+            timer.invalidate()
+            timerLabel.text = "0.00"
+            
+            startButton.isEnabled = true
+            tapButton.isEnabled = false
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tapButton.isEnabled = false
+        
     }
 
     override func didReceiveMemoryWarning() {
